4e70f9a3c0f30d2a75a159ee28e53fc16b17cc4a
PoJu <spes9850401@gmail.com>
2023-08-31 16:30:06 +0800

    Add TickList, ticket details page, and not-found page

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function Navbar() {
   return (
     <nav>
-      <Image src={Logo} alt="Poju Helpdesk Logo" width={70} quality={100} placeholder="blur"/>
+      <Image src={Logo} alt="Poju Helpdesk Logo" height={30} quality={100} placeholder="blur"/>
       <h1>Poju helpdesk</h1>
       <Link href="/">Dashboard</Link>
       <Link href="/tickets">Tickets</Link>
